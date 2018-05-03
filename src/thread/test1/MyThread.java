08143c284662e32fb849f58353835f293f58a852
Mr_He <18335553083@163.com>
2018-05-03 23:58:09 +0800

    init import

---
+++ src/thread/test1/MyThread.java
@@ -0,0 +1,18 @@
+package thread.test1;
+
+/**
+ * @author <a href="mailto:18335553083@163.com">Mr_He</a>
+ * @Copyright (c)</ b> HeC<br/>
+ * @createTime 2018/5/3 22:52
+ * @Description:线程状态判断
+ */
+public class MyThread extends Thread{
+
+    @Override
+    public void run() {
+        super.run();
+
+
+
+    }
+}
---
+++ src/thread/test1/Run.java
@@ -0,0 +1,10 @@
+package thread.test1;
+
+/**
+ * @author <a href="mailto:18335553083@163.com">Mr_He</a>
+ * @Copyright (c)</ b> HeC<br/>
+ * @createTime 2018/5/3 22:53
+ * @Description:
+ */
+public class Run {
+}
